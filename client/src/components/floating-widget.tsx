e26dc1f2233ee990d814c50fd630d5423ab1f865
SwordPayments <sword@mysword.me>
2026-03-06 20:35:53 +0200

    style: modal 90dvh on mobile

--- client/src/components/floating-widget.tsx
+++ client/src/components/floating-widget.tsx
@@ -11,7 +11,7 @@ function FileshareModal({ onClose }: { onClose: () => void }) {
     >
       <div
         className="relative bg-white rounded-2xl shadow-2xl flex flex-col w-[94%] sm:w-[420px] overflow-hidden"
-        style={{ height: '83dvh', maxHeight: '83dvh' }}
+        style={{ height: '90dvh', maxHeight: '90dvh' }}
         onClick={e => e.stopPropagation()}
       >
         {/* Close button */}
