9b70f4e5f62ab1a2fac02fd274ecf46719fbf9b6
SwordPayments <sword@mysword.me>
2026-03-06 22:41:34 +0200

    feat: words 10% faster, widget lower on sm/lg screens

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -134,7 +134,7 @@ export default function Home() {
                         display: 'inline',
                         marginRight: '0.5em',
                         whiteSpace: 'nowrap',
-                        animationDelay: i === 0 ? '0s' : i === 1 ? '2.07s' : '4.14s',
+                        animationDelay: i === 0 ? '0s' : i === 1 ? '1.86s' : '3.73s',
                         animationFillMode: 'backwards',
                       }}
                     >
